3e3c86654e6ed762b7d20fb9cde70bbae3cfaa65
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2024-09-16 01:38:30 +0300

    Upload new problem for 1d arrays.

--- 06-1Dimensional Array/Easy/04-Is It A Palindrome.cpp
+++ 06-1Dimensional Array/Easy/04-Is It A Palindrome.cpp
@@ -1,13 +1,12 @@
 /**
- * @version 0.1
- * @date    15/09/2024
- * @author  Hesham Z. Alsaqqaf.
- * @file    04-Is It A Palindrome.cpp
- * @details Homework Easy [4] : Problem #4: Is It A Palindrome.
- *
- * @copyright Copyright (c) 2024
- */
-
+  * @version   0.1
+  * @date      15/09/2024
+  * @author    Hesham Z. Alsaqqaf | heshamdev2@gmail.com.
+  * @file      Homework Easy [4] : Problem #4: Is It A Palindrome.
+  * @details   Description.
+  *
+  * @copyright Copyright Programmer/Hesham (c) 2024.
+*/
 
 #include <iostream>
 using namespace std;
@@ -16,8 +15,18 @@ int main() {
     const int sizeArray{ 999 };
     int number, arr[sizeArray];
 
-    cout << "Enter Test Case Number";
+    cout << "Enter Test Case Number = ";
     cin >> number;
-    
+
+    for (int i = 0; i < number; ++i)
+        cin >> arr[i];
+
+    for (int i = 0; i < number / 2; ++i) {
+        if (arr[i] != arr[number - 1 - i]) {
+            cout << "NO";
+            return 0; // Stop Program
+        }
+    }
+    cout << "YES";
     return 0;
 }
