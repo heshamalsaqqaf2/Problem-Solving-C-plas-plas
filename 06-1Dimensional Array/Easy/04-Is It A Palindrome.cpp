839d3b44e5b5ecea822e75bf1f9b33e67ccf0f90
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2024-09-15 23:55:25 +0300

    Upload new problem for 1d arrays.

--- 06-1Dimensional Array/Easy/04-Is It A Palindrome.cpp
+++ 06-1Dimensional Array/Easy/04-Is It A Palindrome.cpp
@@ -1,10 +1,9 @@
 /**
- * @file 04-Is It A Palindrome.cpp
- * @author your name (you@domain.com)
- * @brief 
  * @version 0.1
- * @date 2024-09-15
- * 
+ * @date    15/09/2024
+ * @author  Hesham Z. Alsaqqaf.
+ * @file    04-Is It A Palindrome.cpp
+ * @details Homework Easy [4] : Problem #4: Is It A Palindrome.
+ *
  * @copyright Copyright (c) 2024
- * 
  */
