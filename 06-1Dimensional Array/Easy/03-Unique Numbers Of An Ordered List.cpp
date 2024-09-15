26ded3c0dd55682b734f7ffb64b932897bb27656
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2024-09-15 23:51:55 +0300

    Upload new problem for 1d arrays.

---
+++ 06-1Dimensional Array/Easy/03-Unique Numbers Of An Ordered List.cpp
@@ -0,0 +1,49 @@
+/*
+    # Created On 15/09/2024.
+    # Code By: Hesham Z. Alsaqqaf.
+    # Homework Easy [3] : Problem #3: Unique Numbers Of An Ordered List.
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+    /**
+     * @details
+     *        Observe: The Input Remains A Sorted List
+     *        Constraints:
+     *              - Don’t Use Nested Loops!.
+     *              - Using Only 1 Array.
+     */
+    const int sizArray{ 999 };
+    int number, arr[sizArray]; // number > 0
+    
+    cout << "Enter Test Case Number = ";
+    cin >> number;
+
+    for (int i = 0; i < number; ++i) {
+        cin >> arr[i];
+        if (arr[i] != arr[i - 1])
+            cout << arr[i] << " ";
+    }
+
+    /**
+     * @details
+     *        Observe : The Input Remains A Sorted List
+     *        Constraints :
+     *              - Don’t Use Nested Loops!.
+     *              - Try Doing It Without Any Arrays At All.
+     */
+    int number;
+    cout << "Enter Number = ";
+    cin >> number; // number > 0
+
+    int lastValue{ -1 }; // Empty Value Initialized 
+    for (int i = 0; i < number; ++i) {
+        int value;
+        cin >> value;
+        if (value != lastValue)
+            cout << value << " ";
+        lastValue = value;
+    }
+    return 0;
+}
---
+++ 06-1Dimensional Array/Easy/04-Is It A Palindrome.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file 04-Is It A Palindrome.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-09-15
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
